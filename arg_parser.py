ff1cd6aba196536bd992ba2362bcc0ec5cee2a68
Sofia Broomé <sbroome@kth.se>
2017-08-17 16:36:16 +0200

    add arg parsing, might not be the perfect set of args yet

---
+++ arg_parser.py
@@ -0,0 +1,63 @@
+import argparse
+
+args_length = -1
+
+
+class ArgParser:
+    def __init__(self, args_length):
+        self.args_length = args_length
+
+    def parse(self):
+        if self.args_length > 1:
+            args = parse_arguments()
+        else:
+            args = read_default_args()
+        return args
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file-name', nargs='?', type=str,
+                        help="Name of file for data to be processed")
+    parser.add_argument('--model', nargs='?', type=str,
+                        help="Choose neural network architecture.")
+    parser.add_argument('--num-labels', nargs='?', type=int,
+                        help="Number of unique labels for this dataset")
+    parser.add_argument('--nb-lstm-units', nargs='?', type=int,
+                        help="Number of LSTM units")
+    parser.add_argument('--nb-conv-filters', nargs='?', type=int,
+                        help="Number of convolutional filters")
+    parser.add_argument('--nb-epochs', nargs='?', type=int,
+                        help="Number of training epochs")
+    parser.add_argument('--early-stopping', nargs='?', type=int,
+                        help="Early stopping patience")
+    parser.add_argument('--optimizer', nargs='?', type=str,
+                        help="Choice of optimizer (can choose from Keras' different default optimizers)")
+    parser.add_argument('--round-to-batch', nargs='?', type=bool,
+                        help='Choose whether to round the last batch to the specified batch size')
+    parser.add_argument('--device', nargs='?', type=str,
+                        help="Name of device to run on")
+    parser.add_argument('--image-identifier', nargs='?', type=str,
+                        help='Choose some string to identify the image of the training process')
+    return parser.parse_args()
+
+
+def read_default_args():
+    parser = argparse.ArgumentParser()
+    config_file = open("default_args.txt","r")
+    for line in config_file.readlines():
+        key_value_help = line.split(",")
+        try:
+            key = key_value_help[0]
+            value = key_value_help[1]
+            help = key_value_help[2]
+        except Exception, e:
+            print "Invalid format of config file, ignoring..."
+        else:
+            if key == '--file-name' or key == '--devices':
+                parser.add_argument(key, nargs='?', default=value, type=str, help=help)
+            elif key == '--round-to-batch':
+                parser.add_argument(key, nargs='?', default=value, type=bool, help=help)
+            else:
+                parser.add_argument(key, nargs='?', default=value, type=int, help=help)
+    return parser.parse_args()
