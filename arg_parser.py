dca2466eafa6be05e3522c9c2a00aa42b729bfb9
Sofia Broomé <sbroome@kth.se>
2017-08-21 13:38:07 +0200

    add more command line args

--- arg_parser.py
+++ arg_parser.py
@@ -17,22 +17,32 @@ class ArgParser:
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file-name', nargs='?', type=str,
-                        help="Name of file for data to be processed")
+    parser.add_argument('--data-path', nargs='?', type=str,
+                        help="Name of folder with all the data to be processed")
     parser.add_argument('--model', nargs='?', type=str,
                         help="Choose neural network architecture.")
-    parser.add_argument('--num-labels', nargs='?', type=int,
+    parser.add_argument('--input-width', nargs='?', type=int,
+                        help="Input image width")
+    parser.add_argument('--input-height', nargs='?', type=int,
+                        help="Input image height")
+    parser.add_argument('--nb-labels', nargs='?', type=int,
                         help="Number of unique labels for this dataset")
     parser.add_argument('--nb-lstm-units', nargs='?', type=int,
                         help="Number of LSTM units")
     parser.add_argument('--nb-conv-filters', nargs='?', type=int,
                         help="Number of convolutional filters")
+    parser.add_argument('--kernel-size', nargs='?', type=int,
+                        help="Kernel size of convolutional filter")
+    parser.add_argument('--dropout-rate', nargs='?', type=float,
+                        help="Probability of dropout")
     parser.add_argument('--nb-epochs', nargs='?', type=int,
                         help="Number of training epochs")
     parser.add_argument('--early-stopping', nargs='?', type=int,
                         help="Early stopping patience")
     parser.add_argument('--optimizer', nargs='?', type=str,
                         help="Choice of optimizer (can choose from Keras' different default optimizers)")
+    parser.add_argument('--lr', nargs='?', type=float,
+                        help="Learning rate")
     parser.add_argument('--round-to-batch', nargs='?', type=bool,
                         help='Choose whether to round the last batch to the specified batch size')
     parser.add_argument('--device', nargs='?', type=str,
@@ -51,8 +61,8 @@ def read_default_args():
             key = key_value_help[0]
             value = key_value_help[1]
             help = key_value_help[2]
-        except Exception, e:
-            print "Invalid format of config file, ignoring..."
+        except Exception as e:
+            print("Invalid format of config file, ignoring...")
         else:
             if key == '--file-name' or key == '--devices':
                 parser.add_argument(key, nargs='?', default=value, type=str, help=help)
