6ebc9d7b722112014d0cdc111d9b39e228ee6b21
Sofia Broomé <sbroome@kth.se>
2017-08-21 13:39:56 +0200

    add folders to csv method

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,17 @@
-class DataHandler:
-    def __init__(self, path):
-        self.path = path
+import os, csv
 
-    def prepare_data(self):
-        data = []
-        return data
+
+class DataHandler:
+    def __init__(self, path, image_size):
+        self.path = path
+        self.image_size = image_size
+
+    def prepare_train_test(self):
+        pass
+
+    def folders_to_csv(self):
+        with open(self.path + "/file_listing.csv", 'w') as f:
+            writer = csv.writer(f)
+            for path, dirs, files in os.walk(self.path):
+                for filename in files:
+                    writer.writerow([filename])
