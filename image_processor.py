2515919507ed658c3e1f93b48783a9094d5b5683
Sofia Broomé <sbroome@kth.se>
2017-08-22 12:47:23 +0200

    add doc

--- image_processor.py
+++ image_processor.py
@@ -2,14 +2,15 @@ from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 
-def process_image(image, target_shape):
+def process_image(image_path, target_shape):
     """
     Process and image and return the corresponding numerical array.
-    :param image: image
+    :param image_path: str
     :param target_shape: (int, int, int)
     :return: np.ndarray
     """
     # Load the image
     h, w, _ = target_shape
-    img = load_img(image, target_size=target_shape)
+    img = load_img(image_path, target_size=target_shape)
+
     return img_to_array(img).astype(np.float32)
