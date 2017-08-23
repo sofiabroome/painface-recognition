987db49f9bb2f273f14fb49e98e17da843b8ea5c
Sofia Broomé <sbroome@kth.se>
2017-08-23 13:55:16 +0200

    tried with a larger vid

--- vid_to_imseq.sh
+++ vid_to_imseq.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # for file in ~/Dropbox/horse_videos/Experimental_pain/Observer_not_present/No_pain/*.mts; do ffmpeg -i "$file" "${file%.mts}".jpg; done
-ffmpeg -i ~/Dropbox/horse_videos/20120407140353.mts -vf scale=320:-1 -r 1 data/output_%04d.jpg
+ffmpeg -i ~/Dropbox/horse_videos/Experimental_pain/Observer_not_present/No_pain/#1_1b.mts -vf scale=320:-1 -r 1 data/output_%04d.jpg
 
