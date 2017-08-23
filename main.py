2f24bf3be3e815fbbaad9082270541a68a9d8095
Sofia Broomé <sbroome@kth.se>
2017-08-23 13:54:57 +0200

    main so far

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 
 from test_and_eval import Evaluator
-from data_handler import DataHandler
+from data_handler import DataHandler, make_batches
 from train import train
 import arg_parser
 import models
 
-target_names = ['NO_PAIN', 'PAIN']
+TARGET_NAMES = ['NO_PAIN', 'PAIN']
+BATCH_SIZE = 100
+COLOR = True
 
 
 def run(args):
@@ -14,17 +16,19 @@ def run(args):
     model = models.Model(args.model, (args.input_width, args.input_height), seq_length, args.optimizer,
                          args.lr, args.nb_lstm_units, args.nb_conv_filters, args.kernel_size,
                          args.nb_labels, args.dropout_rate)
-    dh = DataHandler(args.data_path, (args.input_width, args.input_height))
-    ev = Evaluator('cr', target_names)
+    dh = DataHandler(args.data_path, (args.input_width, args.input_height), BATCH_SIZE, COLOR)
+    ev = Evaluator('cr', TARGET_NAMES)
 
-    dh.folders_to_csv()
+    # dh.folders_to_csv()
+    df = dh.folders_to_df()
 
     # Prepare the training and testing data
-    # X_train, y_train, X_test, y_test = dh.prepare_data()
-    #
-    # # Train the model
-    # model = train(model, train_args, X_train, y_train)
-    #
+    X_train, y_train, X_test, y_test = dh.prepare_train_test(df)
+    # X_train_batch = make_batches(X_train, BATCH_SIZE)
+
+    # Train the model
+    model = train(model, args, X_train, y_train, batch_size=BATCH_SIZE)
+
     # # Get test predictions
     # y_preds = ev.test(model, X_test)
     #
