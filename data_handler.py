65c2924ae26f8c095f27436703d17d3819999a62
Sofia Broomé <sbroome@kth.se>
2017-08-30 17:02:09 +0200

    add horsetodf and generator data handling

--- data_handler.py
+++ data_handler.py
@@ -1,14 +1,17 @@
+import _pickle as cp
 import pandas as pd
 import numpy as np
-import os, csv
+import os
 
+from keras.utils import np_utils
 from os.path import join
 
+from helpers import split_string_at_last_occurence_of_certain_char
 from image_processor import process_image
 
 
 class DataHandler:
-    def __init__(self, path, image_size, seq_length, color):
+    def __init__(self, path, image_size, seq_length, batch_size, color):
         """
         Constructor for the DataHandler.
         :param path: str
@@ -19,7 +22,58 @@ class DataHandler:
         self.path = path
         self.image_size = image_size
         self.seq_length = seq_length
+        self.batch_size = batch_size
         self.color = color
+
+    def prepare_image_generators(self, df, train):
+        """
+        Prepare the frames into labeled train and test sets, with help from the
+        DataFrame with .jpg-paths and labels for train and pain.
+        :param df: pd.DataFrame
+        :param train: Boolean
+        :return: np.ndarray, np.ndarray, np.ndarray, np.ndarray
+        """
+        if train:
+            df = df.loc[df['Train'] == 1]
+        else:
+            df = df.loc[df['Train'] == 0]
+
+        batch_index = 0
+
+        for index, row in df.iterrows():
+            if batch_index == 0:
+                X_list = []
+                y_list = []
+            x = self.get_image(row['Path'])
+            y = row['Pain']
+            X_list.append(x)
+            y_list.append(y)
+            batch_index += 1
+
+            if batch_index % self.batch_size == 0:
+                X_array = np.array(X_list, dtype=np.float32)
+                y_array = np.array(y_list, dtype=np.uint8)
+                print("**************************************")
+                print("Inside prep image generator:")
+                print("X array shape:")
+                print(X_array.shape)
+                print("y array shape:")
+                print(y_array.shape)
+                y_array = np_utils.to_categorical(y_array, num_classes=2)
+                print(y_array.shape)
+                print("**************************************")
+                # batch_index = 0
+                yield (X_array, y_array)
+
+    def get_image(self, path):
+        if self.color:
+            channels = 3
+        else:
+            channels = 1
+        im = process_image(path, (self.image_size[0], self.image_size[1], channels))
+        return im
+
+    ### BELOW HANDLES DATA EXTRACTION WHEN THE DATA IS ORGANIZED IN TRAIN/TEST/PAIN/NOPAIN-FOLDERS
 
     def prepare_train_test(self, df):
         """
@@ -45,10 +99,13 @@ class DataHandler:
         print(y_test.shape)
         print("**************************************")
 
+        print('Before get train ims')
         train_images_list = self._get_images_from_df(train_df)
+        print('Before get test ims')
         test_images_list = self._get_images_from_df(test_df)
-
+        print('Before convert train to array')
         X_train = np.asarray(train_images_list, dtype=np.float32)
+        print('Before convert test to array')
         X_test = np.asarray(test_images_list, dtype=np.float32)
 
         # X_train_batch = make_even_sequences(train_images_list, self.seq_length)
@@ -92,13 +149,28 @@ class DataHandler:
         df = df[df['FileName'] != '.jpg']
         return df
 
-    def folders_to_csv(self):
-        with open(self.path + "/file_listing.csv", 'w') as f:
-            writer = csv.writer(f)
-            for path, dirs, files in os.walk(self.path):
-                for filename in files:
-                    if '.jpg' in filename:
-                        writer.writerow([filename])
+    def horse_to_df(self, horse_id):
+        """
+        Create a DataFrame with all the frames with annotations from a csv-file.
+        :return: pd.DataFrame
+        """
+        df_csv = pd.read_csv('videos_overview_missingremoved.csv', sep=';')
+        horse_df = pd.DataFrame(columns=['Video_ID', 'Path', 'Pain', 'Observer', 'Train'])
+        c = 0
+        horse_path = self.path + 'horse_' + str(horse_id) + '/'
+        for path, dirs, files in os.walk(horse_path):
+            print(path)
+            for filename in files:
+                total_path = join(path, filename)
+                vid_id = get_video_id_from_path(path)
+                csv_row = df_csv.loc[df_csv['Video_id'] == vid_id]
+                if '.jpg' in filename or '.png' in filename:
+                    train_field = -1
+                    pain_field = csv_row.iloc[0]['Pain']
+                    observer_field = csv_row.iloc[0]['Observer']
+                    horse_df.loc[c] = [vid_id, total_path, pain_field, observer_field, train_field]
+                    c += 1
+        return horse_df
 
     def _get_images_from_df(self, df):
         """
@@ -115,6 +187,14 @@ class DataHandler:
             im = process_image(path, (self.image_size[0], self.image_size[1], channels))
             images.append(im)
         return images
+
+
+def get_video_id_from_path(path):
+    _, vid_id = split_string_at_last_occurence_of_certain_char(path, '/')
+    nb_underscore = vid_id.count('_')
+    if nb_underscore > 1:
+        vid_id, _ = split_string_at_last_occurence_of_certain_char(vid_id, '_')
+    return vid_id
 
 
 def make_even_sequences(x, seq_length):
