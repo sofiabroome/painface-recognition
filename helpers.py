3e6b7dfa7cacf7314d17e88a73cf46c8e281aeb8
Sofia Broomé <sbroome@kth.se>
2017-08-30 17:00:18 +0200

    change function

--- helpers.py
+++ helpers.py
@@ -2,6 +2,6 @@ def get_last_characters_from_string(string, nb_chars):
     return string[-nb_chars:]
 
 
-def get_string_after_last_occurence_of_certain_char(string, char):
+def split_string_at_last_occurence_of_certain_char(string, char):
     left, right = string.rsplit(sep=char, maxsplit=1)
-    return right
+    return left, right
