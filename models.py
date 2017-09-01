2b2d555cfd0bcecf0872026f96c6dc6e0ee6fc02
Sofia Broomé <sbroome@kth.se>
2017-09-01 16:20:16 +0200

    add new conv3d model source albertomontesg

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from keras.layers import Convolution2D, Convolution3D, MaxPooling2D, MaxPooling3D, LSTM, Dense, Flatten
+from keras.layers import ZeroPadding3D, Dropout
 from keras.layers.wrappers import TimeDistributed
 from keras.optimizers import Adam, Adagrad
 from keras.models import Sequential
@@ -136,4 +137,57 @@ class Model:
                         input_shape=(None, self.seq_length, None),
                         return_sequences=False)))
         model.add(Dense(self.nb_labels, activation='softmax'))
+        return model
+
+    def conv3d(self):
+        model = Sequential()
+        # 1st layer group
+        model.add(Convolution3D(64, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv1',
+                                subsample=(1, 1, 1),
+                                input_shape=(self.seq_length, 112, 112, 3)))
+        model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2),
+                               border_mode='valid', name='pool1'))
+        # 2nd layer group
+        model.add(Convolution3D(128, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv2',
+                                subsample=(1, 1, 1)))
+        model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2),
+                               border_mode='valid', name='pool2'))
+        # 3rd layer group
+        model.add(Convolution3D(256, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv3a',
+                                subsample=(1, 1, 1)))
+        model.add(Convolution3D(256, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv3b',
+                                subsample=(1, 1, 1)))
+        model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2),
+                               border_mode='valid', name='pool3'))
+        # 4th layer group
+        model.add(Convolution3D(512, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv4a',
+                                subsample=(1, 1, 1)))
+        model.add(Convolution3D(512, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv4b',
+                                subsample=(1, 1, 1)))
+        model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2),
+                               border_mode='valid', name='pool4'))
+        # 5th layer group
+        model.add(Convolution3D(512, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv5a',
+                                subsample=(1, 1, 1)))
+        model.add(Convolution3D(512, 3, 3, 3, activation='relu',
+                                border_mode='same', name='conv5b',
+                                subsample=(1, 1, 1)))
+        model.add(ZeroPadding3D(padding=(0, 1, 1)))
+        model.add(MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2),
+                               border_mode='valid', name='pool5'))
+        model.add(Flatten())
+        # FC layers group
+        model.add(Dense(4096, activation='relu', name='fc6'))
+        model.add(Dropout(.5))
+        model.add(Dense(4096, activation='relu', name='fc7'))
+        model.add(Dropout(.5))
+        model.add(Dense(487, activation='softmax', name='fc8'))
+        
         return model
