79e05b170b24eef5d6e27bf9fbb265176b64573a
Sofia Broomé <sbroome@kth.se>
2017-09-12 16:00:52 +0200

    add test script

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --data-path 'data/jpg_320_180_1fps/' --model 'conv2d_lstm_informed' --input-width 320 --input-height 180 --nb-labels 2 --nb-lstm-layers 1 --nb-lstm-units 64 --nb-conv-filters 32 --nb-dense-units 512 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 1 --early-stopping 10 --optimizer 'adam' --lr 0.001 --batch-size 50 --round-to-batch True --train-horses '[0,1,2,3]' --test-horses '[4]' --device '/gpu:1' --image-identifier 'withoutH6_t4_jpg_1fps50bs' --test True
