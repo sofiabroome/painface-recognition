568a6564d62aa30a3eaea447995660b62a014670
Sofia Broomé <sbroome@kth.se>
2017-09-13 18:05:54 +0200

    add findbetween string method

--- helpers.py
+++ helpers.py
@@ -5,3 +5,19 @@ def get_last_characters_from_string(string, nb_chars):
 def split_string_at_last_occurence_of_certain_char(string, char):
     left, right = string.rsplit(sep=char, maxsplit=1)
     return left, right
+
+
+def find_between(s, first, last):
+    """
+    Find string between two given strings. First occurence.
+    :param s: whole string [str]
+    :param first: first string [str]
+    :param last: last string [str]
+    :return: the string in between [str]
+    """
+    try:
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
+        return s[start:end]
+    except ValueError:
+        return ""
