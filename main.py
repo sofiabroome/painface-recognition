a385cbd035e25f195cd9deb8ec67f09bc794f1a8
Sofia Broomé <sbroome@kth.se>
2017-09-14 23:09:27 +0200

    cleanup and started writing 2stream handling

--- main.py
+++ main.py
@@ -30,15 +30,27 @@ def df_val_split(df, val_fraction, batch_size, round_to_batch=True):
     return df_train, df_val
 
 
-def run(args):
-    print('batch size:')
+def run():
+
+    # Some initial print outs to keep track of the training mode,
+    # what horses and batch size etc.
+    print('Batch size:')
     print(args.batch_size)
+    print('Sequence length:')
+    print(args.seq_length)
+    print("(Normally they should be the same, except in 5D input mode.)")
+
+    train_horses = ast.literal_eval(args.train_horses)
+    test_horses = ast.literal_eval(args.test_horses)
+    print('Horses to train on: ', train_horses)
+    print('Horses to test on: ', test_horses)
+
     model = models.Model(args)
     dh = DataHandler(args.data_path, (args.input_width, args.input_height),
                      args.seq_length, args.batch_size, COLOR, args.nb_labels)
     ev = Evaluator(True, True, True, TARGET_NAMES, args.batch_size)
 
-    # dh.folders_to_csv()
+    # Read or create the per-horse dataframes listing all the frame paths and labels.
     horse_dfs = []
     for horse in range(1,7):
         print(args.data_path)
@@ -51,37 +63,57 @@ def run(args):
             hdf.to_csv(path_or_buf=horse_csv_path)
         horse_dfs.append(hdf)
 
-    train_horses = ast.literal_eval(args.train_horses)
-    test_horses = ast.literal_eval(args.test_horses)
-    print('Horses to train on: ', train_horses)
-    print('Horses to test on: ', test_horses)
-
     # Set the train-column to 1 (yes) or 0 (no).
     for trh in train_horses:
         horse_dfs[trh]['Train'] = 1
 
     for teh in test_horses:
         horse_dfs[teh]['Train'] = 0
+
     # Put all the separate horse-dfs into one DataFrame.
     df = pd.concat(horse_dfs)
+
     # Shuffle the different sequences (like 1_1a_1) so that they don't always
     # appear in the same order.
     df = shuffle_blocks(df)
-    # pdb.set_trace()
+
+    # Split training data so there is a held out validation set.
 
     df_train, df_val = df_val_split(df, val_fraction=VAL_FRACTION,
-                                    batch_size=args.batch_size, round_to_batch=True)
+                                    batch_size=args.batch_size,
+                                    round_to_batch=args.round_to_batch)
+
+    # Count the number of samples in each partition of the data.
     nb_train_samples = len(df_train)
     nb_val_samples = len(df_val)
     nb_test_samples = len(df[df['Train'] == 0])
 
-    # Prepare the training and testing data 5D
+    # Prepare the training and testing data for 5D-input (batches of videos)
     if 'timedist' in args.model or '5d' in args.model or '3d' in args.model:
         train_generator = dh.prepare_image_generator_5D(df_train, train=True, val=False, test=False, eval=False)
         val_generator = dh.prepare_image_generator_5D(df_val, train=False, val=True, test=False, eval=False)
-        test_generator = dh.prepare_image_generator_5D(df, train=False, val=False, test=True, eval=False)
-        eval_generator = dh.prepare_image_generator_5D(df, train=False, val=False, test=False, eval=True)
+        test_generator = dh.prepare_image_generator_5D(df[df['Train'] == 0], train=False, val=False, test=True, eval=False)
+        eval_generator = dh.prepare_image_generator_5D(df[df['Train'] == 0], train=False, val=False, test=False, eval=True)
 
+    elif '2stream' in args.model:
+        # Read or create the per-horse optical flow files listing all the frame paths and labels.
+        horse_dfs = []
+        for horse in range(1, 7):
+            print(args.data_path)
+            horse_of_csv_path = 'data/jpg_320_180_1fps_OF/horse_' + str(horse) + '.csv'
+            if os.path.isfile(horse_of_csv_path):
+                hdf = pd.read_csv(horse_of_csv_path)
+            else:
+                print('Making a DataFrame for horse id: ', horse)
+                hdf = dh.save_OF_paths_to_df(horse)
+                hdf.to_csv(path_or_buf=horse_of_csv_path)
+            horse_dfs.append(hdf)
+        df_of = None
+        train_generator = dh.prepare_generator_2stream(df_train, df_of, train=True, val=False, test=False, eval=False)
+        val_generator = dh.prepare_generator_2stream(df_val, df_of, train=False, val=True, test=False, eval=False)
+        test_generator = dh.prepare_generator_2stream(df[df['Train'] == 0], df_of, train=False, val=False, test=True, eval=False)
+        eval_generator = dh.prepare_generator_2stream(df[df['Train'] == 0], df_of, train=False, val=False, test=False, eval=True)
+    # Prepare the training and testing data for 4D-input (batches of frames)
     else:        
         train_generator = dh.prepare_train_image_generator(df_train, train=True, val=False, test=False)
         val_generator = dh.prepare_val_image_generator(df_val, train=False, val=True, test=False)
@@ -92,10 +124,12 @@ def run(args):
     model = train(model, args, nb_train_samples, nb_val_samples, VAL_FRACTION,
                   generator=train_generator, val_generator=val_generator)
 
-    # # Get test predictions
+    # Get test predictions
     y_preds, scores = ev.test(model, args, test_generator, eval_generator, nb_test_samples)
 
+    # Get the ground truth for the test set
     y_test = df[df['Train'] == 0]['Pain'].values
+
     # Evaluate the model's performance
     ev.evaluate(model, y_test, y_preds, scores, args)
 
@@ -107,4 +141,4 @@ if __name__ == '__main__':
 
     # Run the whole program, from preparing the data to evaluating
     # the model's test performance
-    run(args)
+    run()
