8e4eb83bc7798af16835f7aa483f2d1d5b2760a0
Sofia Broomé <sbroome@kth.se>
2017-09-15 07:59:45 +0200

    DONT SHUFFLE GENERATOR :wine:

--- data_handler.py
+++ data_handler.py
@@ -160,7 +160,9 @@ class DataHandler:
                     X_array = np.array(X_list, dtype=np.float32)
                     y_array = np.array(y_list, dtype=np.uint8)
                     y_array = np_utils.to_categorical(y_array, num_classes=self.nb_labels)
-                    X_array, y_array = train_datagen.flow(X_array, y_array, batch_size=self.batch_size).next()
+                    X_array, y_array = train_datagen.flow(X_array, y_array,
+                                                          batch_size=self.batch_size,
+                                                          shuffle=False).next()
                     batch_index = 0
                     yield (X_array, y_array)
 
@@ -200,7 +202,9 @@ class DataHandler:
                     X_array = np.array(X_list, dtype=np.float32)
                     y_array = np.array(y_list, dtype=np.uint8)
                     y_array = np_utils.to_categorical(y_array, num_classes=self.nb_labels)
-                    X_array, y_array = val_datagen.flow(X_array, y_array, batch_size=self.batch_size).next()
+                    X_array, y_array = val_datagen.flow(X_array, y_array,
+                                                        batch_size=self.batch_size,
+                                                        shuffle=False).next()
                     batch_index = 0
                     yield (X_array, y_array)
 
@@ -238,7 +242,9 @@ class DataHandler:
                     X_array = np.array(X_list, dtype=np.float32)
                     y_array = np.array(y_list, dtype=np.uint8)
                     y_array = np_utils.to_categorical(y_array, num_classes=self.nb_labels)
-                    X_array, y_array = test_datagen.flow(X_array, y_array, batch_size=self.batch_size).next()
+                    X_array, y_array = test_datagen.flow(X_array, y_array,
+                                                         batch_size=self.batch_size,
+                                                         shuffle=False).next()
                     batch_index = 0
                     yield (X_array, y_array)
 
@@ -276,7 +282,9 @@ class DataHandler:
                     X_array = np.array(X_list, dtype=np.float32)
                     y_array = np.array(y_list, dtype=np.uint8)
                     y_array = np_utils.to_categorical(y_array, num_classes=self.nb_labels)
-                    X_array, y_array = eval_datagen.flow(X_array, y_array, batch_size=self.batch_size).next()
+                    X_array, y_array = eval_datagen.flow(X_array, y_array,
+                                                         batch_size=self.batch_size,
+                                                         shuffle=False).next()
                     batch_index = 0
                     yield (X_array, y_array)
 
