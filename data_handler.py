dd0f26748b3268fef24085b9371b759b2d3acc7f
Sofia Broome <sbroome@kth.se>
2017-09-17 18:14:47 +0200

    gdjfn

--- data_handler.py
+++ data_handler.py
@@ -76,7 +76,6 @@ class DataHandler:
                     y_array = np_utils.to_categorical(y_array, num_classes=self.nb_labels)
                     y_array = np.reshape(y_array, (self.batch_size, self.nb_labels))
                     batch_index = 0
-                    print(X_array.shape, flow_array.shape, y_array.shape)
                     yield [X_array, flow_array], [y_array]
 
     def prepare_image_generator_5D(self, df, train, val, test, eval):
