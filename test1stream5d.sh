48d0c6a08d95cf2a65869d16059a2fd92aef601d
Sofia Broomé <sbroome@kth.se>
2017-09-18 21:33:39 +0200

    test scripts

---
+++ test1stream5d.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --data-path 'data/jpg_320_180_1fps/' --model 'conv2d_timedist_lstm_stateful' --input-width 320 --input-height 180 --nb-labels 2 --nb-lstm-layers 1 --nb-lstm-units 5 --nb-conv-filters 5 --nb-dense-units 64 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 1 --early-stopping 10 --optimizer 'adam' --lr 0.001 --batch-size 20 --seq-length 20 --round-to-batch True --train-horses '[5,0,1,2,3]' --test-horses '[4]' --nb-workers 1 --device '/gpu:1' --image-identifier '1sttest_OF' --test-run 1
