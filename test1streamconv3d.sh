fa53b70f705e44e1853defd37dd3619f2a1373e0
Sofia Broomé <sbroome@kth.se>
2017-09-24 21:57:10 +0200

    some test scripts

---
+++ test1streamconv3d.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py --data-path 'data/jpg_320_180_1fps/' --model 'conv3d_informed' --input-width 320 --input-height 180 --nb-labels 2 --nb-lstm-layers 1 --nb-lstm-units 5 --nb-conv-filters 5 --nb-dense-units 64 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 1 --early-stopping 10 --optimizer 'adam' --lr 0.001 --batch-size 2 --seq-length 100 --round-to-batch True --train-horses '[5,0,1,2]' --val-horses '[3]' --test-horses '[4]' --nb-workers 1 --device '/gpu:1' --image-identifier 'test' --test-run 1 --nb-input-dims 5
