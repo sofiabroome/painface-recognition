4411d69ce22b75e1b86a14b46a9ff6c30038e555
Sofia Broomé <sbroome@kth.se>
2018-02-02 15:03:31 +0100

    coin toss sequence horizontal flipping

--- data_handler.py
+++ data_handler.py
@@ -195,7 +195,9 @@ class DataHandler:
                     y = row['Pain']
                     X_seq_list.append(x)
                     y_seq_list.append(y)
-                X_seq_list = self.flip_images(X_seq_list)
+                coin_toss = np.random.uniform(0, 1)
+                if coin_toss > 0.5:
+                    X_seq_list = self.flip_images(X_seq_list)
                 if batch_index == 0:
                     X_batch_list = []
                     y_batch_list = []
