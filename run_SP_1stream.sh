dc9d7b08a8e5c26b248728f78f867bfa7412b82f
Sofia Broomé <sbroome@kth.se>
2018-03-08 12:59:29 +0100

    runsp

---
+++ run_SP_1stream.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python main.py --data-path 'data/ShoulderPain/Images/' --data-type 'rgb' --model 'convolutional_LSTM' --nb-input-dims 5 --input-width 320 --input-height 240 --nb-labels 2 --nb-lstm-layers 4 --nb-lstm-units 32 --nb-conv-filters 5 --nb-dense-units 64 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 100 --early-stopping 15 --optimizer 'adadelta' --lr 0.001 --batch-size 16 --seq-length 10 --seq-stride 10 --round-to-batch 1 --train-subjects '[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]' --val-subjects '[1]' --val-fraction 0 --test-subjects '[0]' --subjects-overview 'shoulder_pain_subjects.csv' --nb-workers 1 --image-identifier 'SP_1stream_run1_t0' --test-run 0 --aug-flip 0 --aug-crop 0 --aug-light 0
+
