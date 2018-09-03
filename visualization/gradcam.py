b10ac95d134779a065bf4d7524ce1335f89e6e09
Sofia Broome <sbroome@kth.se>
2018-09-03 10:55:07 +0200

    put in main function

--- visualization/gradcam.py
+++ visualization/gradcam.py
@@ -14,14 +14,14 @@ import numpy as np
 import tempfile
 
 from helpers import process_image, find_between
-import visualize_gradcam
+import visualization.visualize_gradcam
 
 
 # Replace vanila relu to guided relu to get guided backpropagation.
-@ops.RegisterGradient("GuidedRelu")
-def _GuidedReluGrad(op, grad):
-    return tf.where(0. < grad, gen_nn_ops._relu_grad(grad, op.outputs[0]),
-                    tf.zeros(grad.get_shape()))
+# @ops.RegisterGradient("GuidedRelu")
+# def _GuidedReluGrad(op, grad):
+#     return tf.where(0. < grad, gen_nn_ops._relu_grad(grad, op.outputs[0]),
+#                     tf.zeros(grad.get_shape()))
 
 class CLSTMNetwork:
     def __init__(self, rgb, from_scratch, path=None):
@@ -123,141 +123,142 @@ class TwoStreamCLSTMNetwork:
         self.dense = m.layers[6](x)
         self.preds = m.layers[7](self.dense)
         return m
+
+if __name__ == '__main__':    
+    width = 128
+    height = 128
+    channels = 3
     
-width = 128
-height = 128
-channels = 3
-
-image_paths = ['../data/jpg_128_128_2fps/horse_2/2_4/frame_000100.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000101.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000102.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000103.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000104.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000105.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000106.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000107.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000108.jpg', 
-               '../data/jpg_128_128_2fps/horse_2/2_4/frame_000109.jpg']
-
-of_paths = ['../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000793.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000801.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000809.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000817.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000825.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000833.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000841.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000849.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000857.jpg',
-            '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000865.jpg']
-
-
-# image_paths = ['../data/jpg_128_128_2fps/horse_2/2_1a/frame_000500.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000501.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000502.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000503.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000504.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000505.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000506.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000507.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000508.jpg', 
-#                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000509.jpg']
-
-nb_ims = len(image_paths)
-horse_id = find_between(image_paths[0], 'fps/', '/')
-video_id = find_between(image_paths[0], horse_id + '/', '/frame')
-metadata = pd.read_csv('../metadata/videos_overview_missingremoved.csv', sep=';')
-pain = metadata[metadata['Video_ID']==video_id]['Pain'].values[0]
-
-# best_model_path = '../models/BEST_MODEL_convolutional_LSTM_adadelta_LSTMunits_6_CONVfilters_5_test5d.h5'
-# best_model_path = '../models/BEST_MODEL_convolutional_LSTM_adadelta_LSTMunits_32_CONVfilters_None_jpg128_2fps_val4_t1_seq10ss10_4hl_32ubs16_no_aug_june24th.h5'
-best_model_path = '../models/BEST_MODEL_2stream_5d_adadelta_LSTMunits_32_CONVfilters_16_add_v4_t0_4hl_128jpg2fps_seq10_bs8_adadelta_noaug_run1_rerun_for_gradcam.h5'
-
-if pain:
-    label_onehot = np.array([1 if i == 1 else 0 for i in range(2)])
-else:
-    label_onehot = np.array([1 if i == 0 else 1 for i in range(2)])
-
-label_onehot = label_onehot.reshape(1,1,-1)
-batch_label = np.asarray(nb_ims*[label_onehot]).reshape(1, nb_ims, -1)
-
-imgs = []
-for img_path in image_paths:
-    img = process_image(img_path, (width, height, channels))
-    img = img.reshape((1, 1, width, height, channels))
-    imgs.append(img)
-
-batch_img = np.concatenate(imgs, axis=1)
-batch_size = nb_ims
-
-flows = []
-for flow_path in of_paths:
-    flow = process_image(flow_path, (width, height, channels))
-    flow = flow.reshape((1, 1, width, height, channels))
-    flows.append(flow)
-
-batch_flow = np.concatenate(flows, axis=1)
-# DEFINE GRAPH
-
-from keras import backend as K
-
-images = tf.placeholder(tf.float32, [1, batch_size, width, height, channels])
-flows = tf.placeholder(tf.float32, [1, batch_size, width, height, channels])
-labels = tf.placeholder(tf.float32, [1, batch_size, 2])
-
-# clstm_model = CLSTMNetwork(images, from_scratch=0, path=best_model_path)
-clstm_model = TwoStreamCLSTMNetwork(images, flows, from_scratch=0, path=best_model_path)
-
-sess = K.get_session()  # Grab the Keras session where the weights are initialized.
-
-cost = (-1) * tf.reduce_sum(tf.multiply(labels, tf.log(clstm_model.preds)), axis=1)
-
-y_c = tf.reduce_sum(tf.multiply(clstm_model.dense, labels), axis=1)
-
-# target_conv_layer = clstm_model.merge # Choose which CLSTM-layer to study
-# target_conv_layer = clstm_model.clstm4 # Choose which CLSTM-layer to study
-target_conv_layer = clstm_model.clstm1_rgb # Choose which CLSTM-layer to study
-
-target_conv_layer_grad = tf.gradients(y_c, target_conv_layer)[0]
-
-gb_grad = tf.gradients(cost, [images, flows])[0]  # Guided backpropagation back to input layer
-
-
-with sess.as_default():
-    # 2-stream
-    prob = sess.run(clstm_model.preds,
-                    feed_dict={images: batch_img,
-                               flows: batch_flow,
-                               K.learning_phase(): 0})
-
-    print(prob)
-
-    gb_grad_value, target_conv_layer_value, target_conv_layer_grad_value = \
-        sess.run([gb_grad, target_conv_layer, target_conv_layer_grad],
-                  feed_dict={images: batch_img,
-                             flows: batch_flow,
-                             labels: batch_label,
-                             K.learning_phase(): 0})
-    # target_conv_layer_value = np.reshape(target_conv_layer_value,
-    #                                      (1, batch_size, 8, 8, 32))
-    # target_conv_layer_grad_value = np.reshape(target_conv_layer_grad_value,
-    #                                      (1, batch_size, 8, 8, 32))
-    # 1-stream
-    # prob = sess.run(clstm_model.preds,
-    #                 feed_dict={images: batch_img,
-    #                            K.learning_phase(): 0})
-
-    # print(prob)
-    # gb_grad_value, target_conv_layer_value, target_conv_layer_grad_value = \
-    #     sess.run([gb_grad, target_conv_layer, target_conv_layer_grad],
-    #               feed_dict={images: batch_img,
-    #               labels: batch_label,
-    #               K.learning_phase(): 0})
-
-    # import pdb; pdb.set_trace()
-    for i in range(batch_size):
-        print(prob[0,i,:])
-        visualize_gradcam.visualize(batch_flow[:,i,:,:],
-                                    target_conv_layer_value[:,i,:,:],
-                                    target_conv_layer_grad_value[:,i,:,:],
-                                    gb_grad_value[:,i,:,:], number=i)
-
+    image_paths = ['../data/jpg_128_128_2fps/horse_2/2_4/frame_000100.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000101.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000102.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000103.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000104.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000105.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000106.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000107.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000108.jpg', 
+                   '../data/jpg_128_128_2fps/horse_2/2_4/frame_000109.jpg']
+    
+    of_paths = ['../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000793.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000801.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000809.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000817.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000825.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000833.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000841.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000849.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000857.jpg',
+                '../data/jpg_128_128_16fps_OF_magnitude_cv2/horse_2/2_4/flow_000865.jpg']
+    
+    
+    # image_paths = ['../data/jpg_128_128_2fps/horse_2/2_1a/frame_000500.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000501.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000502.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000503.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000504.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000505.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000506.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000507.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000508.jpg', 
+    #                '../data/jpg_128_128_2fps/horse_2/2_1a/frame_000509.jpg']
+    
+    nb_ims = len(image_paths)
+    horse_id = find_between(image_paths[0], 'fps/', '/')
+    video_id = find_between(image_paths[0], horse_id + '/', '/frame')
+    metadata = pd.read_csv('../metadata/videos_overview_missingremoved.csv', sep=';')
+    pain = metadata[metadata['Video_ID']==video_id]['Pain'].values[0]
+    
+    # best_model_path = '../models/BEST_MODEL_convolutional_LSTM_adadelta_LSTMunits_6_CONVfilters_5_test5d.h5'
+    # best_model_path = '../models/BEST_MODEL_convolutional_LSTM_adadelta_LSTMunits_32_CONVfilters_None_jpg128_2fps_val4_t1_seq10ss10_4hl_32ubs16_no_aug_june24th.h5'
+    best_model_path = '../models/BEST_MODEL_2stream_5d_adadelta_LSTMunits_32_CONVfilters_16_add_v4_t0_4hl_128jpg2fps_seq10_bs8_adadelta_noaug_run1_rerun_for_gradcam.h5'
+    
+    if pain:
+        label_onehot = np.array([1 if i == 1 else 0 for i in range(2)])
+    else:
+        label_onehot = np.array([1 if i == 0 else 1 for i in range(2)])
+    
+    label_onehot = label_onehot.reshape(1,1,-1)
+    batch_label = np.asarray(nb_ims*[label_onehot]).reshape(1, nb_ims, -1)
+    
+    imgs = []
+    for img_path in image_paths:
+        img = process_image(img_path, (width, height, channels))
+        img = img.reshape((1, 1, width, height, channels))
+        imgs.append(img)
+    
+    batch_img = np.concatenate(imgs, axis=1)
+    batch_size = nb_ims
+    
+    flows = []
+    for flow_path in of_paths:
+        flow = process_image(flow_path, (width, height, channels))
+        flow = flow.reshape((1, 1, width, height, channels))
+        flows.append(flow)
+    
+    batch_flow = np.concatenate(flows, axis=1)
+    # DEFINE GRAPH
+    
+    from keras import backend as K
+    
+    images = tf.placeholder(tf.float32, [1, batch_size, width, height, channels])
+    flows = tf.placeholder(tf.float32, [1, batch_size, width, height, channels])
+    labels = tf.placeholder(tf.float32, [1, batch_size, 2])
+    
+    # clstm_model = CLSTMNetwork(images, from_scratch=0, path=best_model_path)
+    clstm_model = TwoStreamCLSTMNetwork(images, flows, from_scratch=0, path=best_model_path)
+    
+    sess = K.get_session()  # Grab the Keras session where the weights are initialized.
+    
+    cost = (-1) * tf.reduce_sum(tf.multiply(labels, tf.log(clstm_model.preds)), axis=1)
+    
+    y_c = tf.reduce_sum(tf.multiply(clstm_model.dense, labels), axis=1)
+    
+    # target_conv_layer = clstm_model.merge # Choose which CLSTM-layer to study
+    # target_conv_layer = clstm_model.clstm4 # Choose which CLSTM-layer to study
+    target_conv_layer = clstm_model.clstm1_rgb # Choose which CLSTM-layer to study
+    
+    target_conv_layer_grad = tf.gradients(y_c, target_conv_layer)[0]
+    
+    gb_grad = tf.gradients(cost, [images, flows])[0]  # Guided backpropagation back to input layer
+    
+    
+    with sess.as_default():
+        # 2-stream
+        prob = sess.run(clstm_model.preds,
+                        feed_dict={images: batch_img,
+                                   flows: batch_flow,
+                                   K.learning_phase(): 0})
+    
+        print(prob)
+    
+        gb_grad_value, target_conv_layer_value, target_conv_layer_grad_value = \
+            sess.run([gb_grad, target_conv_layer, target_conv_layer_grad],
+                      feed_dict={images: batch_img,
+                                 flows: batch_flow,
+                                 labels: batch_label,
+                                 K.learning_phase(): 0})
+        # target_conv_layer_value = np.reshape(target_conv_layer_value,
+        #                                      (1, batch_size, 8, 8, 32))
+        # target_conv_layer_grad_value = np.reshape(target_conv_layer_grad_value,
+        #                                      (1, batch_size, 8, 8, 32))
+        # 1-stream
+        # prob = sess.run(clstm_model.preds,
+        #                 feed_dict={images: batch_img,
+        #                            K.learning_phase(): 0})
+    
+        # print(prob)
+        # gb_grad_value, target_conv_layer_value, target_conv_layer_grad_value = \
+        #     sess.run([gb_grad, target_conv_layer, target_conv_layer_grad],
+        #               feed_dict={images: batch_img,
+        #               labels: batch_label,
+        #               K.learning_phase(): 0})
+    
+        # import pdb; pdb.set_trace()
+        for i in range(batch_size):
+            print(prob[0,i,:])
+            visualize_gradcam.visualize(batch_flow[:,i,:,:],
+                                        target_conv_layer_value[:,i,:,:],
+                                        target_conv_layer_grad_value[:,i,:,:],
+                                        gb_grad_value[:,i,:,:], number=i)
+    
