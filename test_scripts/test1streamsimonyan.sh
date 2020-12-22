e509197b3ae4830e96587534cfa72ed062dd108b
Sofia Broome <broome.sofia@gmail.com>
2020-12-22 13:04:36 +0000

    rm test scripts folder, not relevant in current state of repo

--- test_scripts/test1streamsimonyan.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --data-path 'data/jpg_320_180_1fps/' --model 'simonyan' --input-width 320 --input-height 180 --nb-labels 2 --nb-lstm-layers 1 --nb-lstm-units 5 --nb-conv-filters 5 --nb-dense-units 64 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 1 --early-stopping 10 --optimizer 'adam' --lr 0.001 --batch-size 20 --seq-length 20 --round-to-batch True --train-horses '[5,0,1,2]' --val-horses '[3]' --test-horses '[4]' --nb-workers 1 --device '/gpu:1' --image-identifier 'test' --test-run 1 --nb-input-dims 5
--- test_scripts/testOF.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python main.py --data-path 'data/jpg_320_180_1fps/' --model '2stream' --input-width 320 --input-height 180 --nb-labels 2 --nb-lstm-layers 1 --nb-lstm-units 64 --nb-conv-filters 32 --nb-dense-units 512 --kernel-size 5 --dropout-1 0.25 --dropout-2 0.5 --nb-epochs 1 --early-stopping 10 --optimizer 'adam' --lr 0.001 --batch-size 50 --seq-length 50 --round-to-batch True --train-horses '[5,0,1,2,3]' --test-horses '[4]' --device '/gpu:1' --image-identifier '1sttest_OF' --test-run 1
