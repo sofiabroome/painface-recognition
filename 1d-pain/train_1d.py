22da9d668ec717f37f7ed7ef76cd2b6be52f0290
Sofia Broome <broome.sofia@gmail.com>
2021-02-15 19:22:11 +0000

    log tv things

--- 1d-pain/train_1d.py
+++ 1d-pain/train_1d.py
@@ -37,10 +37,10 @@ def train_1d(train_dataset, val_dataset, model, optimizer, config_dict):
         return loss
 
 
-    # @tf.function(input_signature=(
-    #     tf.TensorSpec(shape=[config_dict['batch_size'], config_dict['video_pad_length']], dtype=tf.float32),
-    #     tf.TensorSpec(shape=[config_dict['batch_size'], config_dict['video_pad_length'], 2], dtype=tf.int32),
-    #     tf.TensorSpec(shape=[config_dict['batch_size'],], dtype=tf.int32)))
+    @tf.function(input_signature=(
+        tf.TensorSpec(shape=[config_dict['batch_size'], config_dict['video_pad_length']], dtype=tf.float32),
+        tf.TensorSpec(shape=[config_dict['batch_size'], config_dict['video_pad_length'], 2], dtype=tf.int32),
+        tf.TensorSpec(shape=[config_dict['batch_size'],], dtype=tf.int32)))
     def train_step(x, y, length):
         with tf.GradientTape() as tape:
             if config_dict['model_name'] == 'transformer':
@@ -62,12 +62,12 @@ def train_1d(train_dataset, val_dataset, model, optimizer, config_dict):
         optimizer.apply_gradients(zip(grads, model.trainable_weights))
         train_acc_metric.update_state(y, preds_mil)
         # train_acc_metric.update_state(y, preds_seq)
-        return loss
+        return loss, tv_p, tv_np, mil
         
-    # @tf.function(input_signature=(
-    #     tf.TensorSpec(shape=[config_dict['val_batch_size'], config_dict['video_pad_length']], dtype=tf.float32),
-    #     tf.TensorSpec(shape=[config_dict['val_batch_size'], config_dict['video_pad_length'], 2], dtype=tf.int32),
-    #     tf.TensorSpec(shape=[config_dict['val_batch_size'],], dtype=tf.int32)))
+    @tf.function(input_signature=(
+        tf.TensorSpec(shape=[config_dict['val_batch_size'], config_dict['video_pad_length']], dtype=tf.float32),
+        tf.TensorSpec(shape=[config_dict['val_batch_size'], config_dict['video_pad_length'], 2], dtype=tf.int32),
+        tf.TensorSpec(shape=[config_dict['val_batch_size'],], dtype=tf.int32)))
     def val_step(x, y, length):
         if config_dict['model_name'] == 'transformer':
             preds_seq = model([x, y])
@@ -83,21 +83,27 @@ def train_1d(train_dataset, val_dataset, model, optimizer, config_dict):
         val_acc_metric.update_state(y, preds_mil)
     #     loss = loss_fn(y, preds_seq)
     #     val_acc_metric.update_state(y, preds_seq)
-        return loss
+        return loss, tv_p, tv_np, mil
 
     for epoch in range(config_dict['epochs']):
         wandb.log({'epoch': epoch})
         for x, y, length in train_dataset:
             # train_loss = train_step_crossentropy(x, y, length)
-            train_loss = train_step(x, y, length)
+            train_loss, tv_p, tv_np, mil = train_step(x, y, length)
             wandb.log({'train_loss': train_loss})
+            wandb.log({'tv_nopain': tv_np})
+            wandb.log({'tv_pain': tv_p})
+            wandb.log({'mil': mil})
         train_acc = train_acc_metric.result()
         wandb.log({'train_acc': train_acc})
         
         for x, y, length in val_dataset:
             # val_loss = val_step_crossentropy(x, y, length)
-            val_loss = val_step(x, y, length)
+            val_loss, val_tv_p, val_tv_np, val_mil = val_step(x, y, length)
             wandb.log({'val_loss': val_loss})
+            wandb.log({'val_tv_nopain': val_tv_np})
+            wandb.log({'val_tv_pain': val_tv_p})
+            wandb.log({'val_mil': val_mil})
         val_acc = val_acc_metric.result()
         wandb.log({'val_acc': val_acc})
         if epoch % 20 == 0:
