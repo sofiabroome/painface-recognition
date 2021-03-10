f7a7c8c696091f3d4a54839492c23d02e607f7ba
Sofia Broomé <sbroome@kth.se>
2021-03-10 08:44:42 +0100

    224 scripts for interpretability and human study frame extraction

---
+++ data_scripts/extract_frames_from_human_clips_lps224.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import subprocess
+import os
+
+
+if __name__ == '__main__':
+    fps_to_extract = 2
+    overview_df = pd.read_csv('../metadata/lps_videos_overview.csv', sep=',')
+    df = pd.read_csv('../data/lps/random_clips_lps/ground_truth_randomclips_lps.csv', sep=',')
+    root_dir = '../data/lps/random_clips_lps/'
+    frames_dir = '../data/lps/random_clips_lps/jpg_224_224_{}fps/'.format(fps_to_extract)
+
+    if not os.path.exists(frames_dir):
+        subprocess.call(['mkdir', frames_dir])
+
+    # Create one folder per test clip
+    for index, row in df.iterrows():
+        print("NEW CLIP FROM STUDY, mkdir")
+        clip_index = row['ind']
+        tc_x = 'test_clip_' + str(clip_index)
+        output_dir_path = os.path.join(frames_dir, tc_x)
+        if not os.path.exists(output_dir_path):
+            subprocess.call(['mkdir', output_dir_path])
+
+    # Extract frames
+
+    start = '00:00:00'  # Use whole videos here
+    for index, row in df.iterrows():
+        print("\nNEW CLIP FROM STUDY, extracting frames...")
+        clip_index = row['ind']
+        tc_x = 'test_clip_' + str(clip_index)
+        filename = tc_x + '.mp4' 
+        output_dir = tc_x
+
+        seq_dir_path = os.path.join(frames_dir, output_dir)
+
+        # Start as hh:mm:ss-strings, length as number of seconds
+        length = str(5)
+        print(start)
+
+        complete_output_path = os.path.join(seq_dir_path, 'frame_%06d.jpg')
+        video_path = os.path.join(root_dir, filename)
+
+        print('COMPLETE OUTPUT PATH:')
+        print(complete_output_path)
+        print('VIDEO PATH:')
+        print(video_path)
+
+        if not os.path.exists(complete_output_path):
+            print("comp path does not exist")
+
+        if not os.path.exists(seq_dir_path):
+            print("seq path does not exist")
+
+        if not os.path.exists(video_path):
+            print("video path does not exist")
+
+        print(os.environ['PATH'])
+
+        # JPG {}FPS 224x224
+
+        ffmpeg_command = ['ffmpeg', '-ss', start, '-i', video_path, '-qscale:v', str(4), '-t', length, '-vf',
+                          'scale=224:224', '-r', str(fps_to_extract), '-an', complete_output_path]
+
+        print(ffmpeg_command)
+        subprocess.call(ffmpeg_command)
+
---
+++ data_scripts/optical_flow_test_clips224.py
@@ -0,0 +1,111 @@
+import sys
+sys.path.append('../') # Adds higher directory to python modules path.
+from helpers import process_image, find_between
+import pandas as pd
+import subprocess
+import argparse
+import os
+
+import optical_flow
+
+pd.set_option('max_colwidth', 800)
+
+# Flow Options:
+alpha = 0.012
+ratio = 0.5625
+minWidth = 20
+nOuterFPIterations = 7
+nInnerFPIterations = 1
+nSORIterations = 30
+colType = 0  # 0 or default:RGB, 1:GRAY (but pass gray image with shape (h,w,1))
+
+parser = argparse.ArgumentParser(
+    description='Python wrapper of Coarse2Fine Optical Flow')
+parser.add_argument(
+    '-viz', dest='viz', action='store_true',
+    help='Visualize (i.e. save) output of flow.')
+args = parser.parse_args()
+
+
+def make_folders():
+    """
+    This method only needs to be run once. It creates the per-sequence
+    folders where to save the computed optical flow.
+    :return: None
+    """
+    for index, row in df.iterrows():
+        print("NEW CLIP FROM STUDY, mkdir")
+        clip_index = row['ind']
+        tc_x = 'test_clip_' + str(clip_index)
+        seq_dir_path = os.path.join(output_dir, tc_x)
+        if not os.path.exists(seq_dir_path):
+            subprocess.call(['mkdir', seq_dir_path])
+
+
+def iterate_over_frames(frequency):
+
+    frames_df = pd.read_csv(frames_csv_path, sep=',')
+    counter = 0
+    per_video_counter = 0
+    # Every row in the df contains 1 video frame.
+    for row in frames_df.iterrows():
+        if counter == 0:
+            # The ims list will always contain maximum 2 images, between
+            # which images the optical flow will be computed.
+            ims = []
+        if counter >= 2:
+            if old_vid_seq_name != vid_seq_name:
+                print('New clip!', counter, per_video_counter)
+                print(vid_seq_name)
+                per_video_counter = 0
+                old_vid_seq_name = vid_seq_name
+            # -1 if I want to start at 1, otherwise 2.
+            counter_format = ("%06d" % (per_video_counter-1))
+            frequency_counter = per_video_counter % frequency
+            if frequency_counter == 2 or frequency_counter == 14:
+                flow_output_path_stem = os.path.join(
+                    output_dir, vid_seq_name, 'flow_' + counter_format)
+                print(flow_output_path_stem)
+                optical_flow.compute_optical_flow(ims, flow_output_path_stem, magnitude=True)
+            ims[0] = ims[1]
+            ims.pop()
+        frame_path = row[1]['path']
+        # print(frame_path)
+        # Equine data
+        vid_seq_name = find_between(frame_path, 'fps/', '/frame')
+        # print(vid_seq_name)
+        # print('\n')
+        im = process_image(frame_path, (width, height, channels), standardize=False,
+                           mean=None, std=None, normalize=False)
+        ims.append(im)
+        counter += 1
+        per_video_counter += 1
+        if counter == 1:
+            print('First clip to flow from: ', vid_seq_name)
+            old_vid_seq_name = vid_seq_name
+
+
+if __name__ == '__main__':
+    # CSV with info about all the video sequences.
+    df = pd.read_csv('../data/lps/random_clips_lps/ground_truth_randomclips_lps.csv', sep=',')
+
+    # Directory with the frames from which to extract the OF.
+    frames_dir = '../data/lps/random_clips_lps/jpg_224_224_25fps/'
+    frames_csv_path = frames_dir + 'test_clip_frames.csv'
+
+    # Output root directory (will contain subfolders for every sequence).
+    # Need to make this folder before running, and the horse_x folders in it.
+    output_dir = '../data/lps/random_clips_lps/jpg_224_224_25fps_OF_magnitude_2fpsrate/'
+    if not os.path.exists(output_dir):
+        subprocess.call(['mkdir', output_dir])
+
+    width = 224
+    height = 224
+    channels = 3
+
+    # Only need to make the subfolders of output_root_dir once.
+    make_folders()
+
+    # Iterate over the frames in root_dir and compute the flows.
+    # Right now this is done for every 15th frame.
+    iterate_over_frames(frequency=25)
